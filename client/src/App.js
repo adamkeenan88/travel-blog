ddc85e917a177fa3001f163402a881006b839e19
admin <admin@admins-MacBook-Pro.local>
2022-05-05 10:41:34 -0700

    setting up folders for views and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import Map from "./Components/mapaspect";
 import Login from "./Components/login";
 import "./App.css";
+import { Link } from "@reach/router";
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <Map />
     </div>
   );
 }
