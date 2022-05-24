13f39eab116cd043d8ca93357e1aa52792936572
admin <admin@admins-MacBook-Pro.local>
2022-05-24 13:05:49 -0700

    updated server with routes, JWT, cookie-parser, and updared routes and controller logic

---
+++ server/routes/user.routes.js
@@ -0,0 +1,9 @@
+const Users = require("../controllers/user.controller");
+const { authenticate } = require("../config/jwt.config");
+
+module.exports = (app) => {
+  app.post("/api/register", Users.register);
+  app.post("/api/login", Users.login);
+  // this route now has to be authenticated
+  app.get("/api/users", authenticate, Users.getAll);
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+app.use(express);
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.listen(port, () => console.log(`App listening on port ${port}`));
